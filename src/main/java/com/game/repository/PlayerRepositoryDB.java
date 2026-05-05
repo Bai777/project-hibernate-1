e83c5df5f2fb16b7b7f14cba36daf9e86bf2e752
Чат бот <bai_777@mail.ru>
2026-05-05 13:55:42 +0300

    вынес настройки подключение к БД в hibernate.properties

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -5,29 +5,19 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
-import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
     private final SessionFactory sessionFactory;
 
     public PlayerRepositoryDB() {
-        Properties properties = new Properties();
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
-        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
-        properties.put(Environment.URL, "jdbc:p6spy:postgresql://localhost:5432/postgres");
-        properties.put(Environment.USER, "postgres");
-        properties.put(Environment.PASS, "admin");
-        properties.put(Environment.HBM2DDL_AUTO, "update");
         sessionFactory = new Configuration()
                 .addAnnotatedClass(Player.class)
-                .addProperties(properties)
                 .buildSessionFactory();
     }
 
